2460d09e860118cb4542c43b8f9efd8edc149805
Ersultan <ersultansansyzbaj012@gmail.com>
2022-10-06 20:34:49 +0600

    useMemo

--- src/App.js
+++ src/App.js
@@ -1,87 +1,23 @@
-import { useState, useCallback } from "react";
+import { useState, useCallback, useEffect, useMemo } from "react";
 import { Container } from "react-bootstrap";
 import "./App.css";
-// class Slider extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       autoplay: false,
-//       slide: 0,
-//     };
-//   }
 
-//   componentDidMount() {
-//     document.title = `Slide: ${this.state.slide}`;
-//   }
-
-//   componentDidUpdate() {
-//     document.title = `Slide: ${this.state.slide}`;
-//   }
-
-//   changeSlide = (i) => {
-//     this.setState(({ slide }) => ({
-//       slide: slide + i,
-//     }));
-//   };
-
-//   toggleAutoplay = () => {
-//     this.setState(({ autoplay }) => ({
-//       autoplay: !autoplay,
-//     }));
-//   };
-
-//   render() {
-//     return (
-//       <Container>
-//         <div className="slider w-50 m-auto">
-//           <img
-//             className="d-block w-100"
-//             src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg"
-//             alt="slide"
-//           />
-//           <div className="text-center mt-5">
-//             Active slide {this.state.slide} <br />{" "}
-//             {this.state.autoplay ? "auto" : null}
-//           </div>
-//           <div className="buttons mt-3">
-//             <button
-//               className="btn btn-primary me-2"
-//               onClick={() => this.changeSlide(-1)}
-//             >
-//               -1
-//             </button>
-//             <button
-//               className="btn btn-primary me-2"
-//               onClick={() => this.changeSlide(1)}
-//             >
-//               +1
-//             </button>
-//             <button
-//               className="btn btn-primary me-2"
-//               onClick={this.toggleAutoplay}
-//             >
-//               toggle autoplay
-//             </button>
-//           </div>
-//         </div>
-//       </Container>
-//     );
-//   }
-// }
-
-const getSomeImages = () => {
-  console.log("fetching");
-  return [
-    "https://r19.ru/upload/medialibrary/3b8/2gis.jpg",
-    "https://play-lh.googleusercontent.com/TzCVWcsxIQ5khGo-B9HTLflp-b9mXg0ILseLuCztU2XfvbVnD8W0cwNc1YHVtCGXhQBW=w526-h296-rw",
-  ];
+const countTotal = (num) => {
+  console.log("counting...");
+  return num + 10;
 };
 
 const Slider = (props) => {
   const [slide, setSlide] = useState(0);
   const [autoplay, setAutoplay] = useState(false);
 
-  const getSomeImages = useCallback()
+  const getSomeImages = useCallback(() => {
+    console.log("fetching");
+    return [
+      "https://r19.ru/upload/medialibrary/3b8/2gis.jpg",
+      "https://play-lh.googleusercontent.com/TzCVWcsxIQ5khGo-B9HTLflp-b9mXg0ILseLuCztU2XfvbVnD8W0cwNc1YHVtCGXhQBW=w526-h296-rw",
+    ];
+  }, [slide]);
 
   function changeSlide(i) {
     setSlide((slide) => slide + i);
@@ -91,18 +27,33 @@ const Slider = (props) => {
     setAutoplay((autoplay) => !autoplay);
   }
 
+  const total = useMemo(() => {
+    return countTotal(slide);
+  }, [slide]);
+
+  const style = useMemo(
+    () => ({
+      color: slide > 4 ? "red" : "black",
+    }),
+    [slide]
+  );
+
+  useEffect(() => {
+    console.log("style");
+  }, [style]);
+
   return (
     <Container>
       <div className="slider w-50 m-auto">
-        {getSomeImages().map((url, i) => {
-          return (
-            <img key={i} className="d-block w-100" src={url} alt="slide" />
-          );
-        })}
+        <Slide getSomeImages={getSomeImages} />
 
         <div className="text-center mt-5">
           Active slide {slide} <br />
           {autoplay ? "auto" : null}
+        </div>
+
+        <div style={style} className="text-center mt-5">
+          Total slides: {total}
         </div>
         <div className="buttons mt-3">
           <button
@@ -123,6 +74,22 @@ const Slider = (props) => {
         </div>
       </div>
     </Container>
+  );
+};
+
+const Slide = ({ getSomeImages }) => {
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    setImages(getSomeImages());
+  }, [getSomeImages]);
+
+  return (
+    <>
+      {images.map((url, i) => (
+        <img key={i} className="d-block w-100" src={url} alt="slide" />
+      ))}
+    </>
   );
 };
 
